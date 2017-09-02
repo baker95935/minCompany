4c2df4939e78d3f99268e2191ab4d8645cf32b52
baker95935 <baker95935@qq.com>
2017-09-02 13:34:09 +0800

    count is ok!

--- application/admin/controller/Count.php
+++ application/admin/controller/Count.php
@@ -2,85 +2,102 @@
 
 namespace app\admin\controller;
 
-use think\Controller;
+use app\admin\controller\Common;
 use think\Request;
+use app\admin\model\Count as countModel;
 
 //统计信息
-class Count extends Controller
+class Count extends Common
 {
-    /**
-     * 显示资源列表
-     *
-     * @return \think\Response
-     */
-    public function index()
-    {
-        //
-    }
+ 	public function index()
+	{
+		$list=array();
+		$count=new countModel();
+		
+		$request = request();
+		$search['id']=$request->param('id');
+		$search['username']=$request->param('username');
+		$search['ipaddr']=$request->param('ipaddr');
+		$search['start_time']=$request->param('start_time');
+		$search['end_time']=$request->param('end_time');
+	 
+	 
+		!empty($search['id']) && $where['id']=['=',$search['id']];
+		!empty($search['username']) && $where['username']=['like',"%".$search['username']."%"];
+		!empty($search['ipaddr']) && $where['ipaddr']=['like',"%".$search['ipaddr']."%"];
+		!empty($search['start_time']) && $where['start_time']=['like',"%".$search['start_time']."%"];
+		!empty($search['end_time']) && $where['end_time']=['like',"%".$search['end_time']."%"];
+		
+		$where['id']=['>',0];
+		
+		$list=$count->getListInfo($where,array('search'=>$search));
+		$this->assign('list',$list);
+		$this->assign('search',$search);
+		
+		return view();
+	}
 
-    /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-        //
-    }
-
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
-
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
-    }
+	public function add()
+	{
+		$count=new countModel();
+		$request = request();
+		
+		$id=$request->param('id');
+	 
+		if($request->method()=='POST') {
+			//数据获取
+			$data=array(
+				'username'=>$request->param('username'),
+			    'ipaddr'=>$request->param('ipaddr'),
+				'start_time'=>strtotime($request->param('start_time')),
+				'end_time'=>strtotime($request->param('end_time')),
+				'status'=>$request->param('status'),
+				'id'=>$request->param('id'),
+			);
+			
+		 
+				
+			$result=0;
+			if(empty($id)){//添加
+				$data['create_time']=time();
+				$result=$count->addInfo($data);
+			} else {
+				$result=$count->addInfo($data,array('id'=>$id));//更新
+			}
+			
+			if($result) {
+				$this->success('操作成功', '/admin/count/index/');
+			} else {
+				$this->success('操作失败或未生效请重试', '/admin/count/index/');
+			}
+	
+		}
+			
+		$data=array();
+		!empty($id) && $data=$count::get($id);
+ 
+		$this->assign('data',$data);
+		return view();
+	}
+	
+	public function del()
+	{
+		$count=new countModel();
+		$request = request();
+		
+		if($request->method()=='GET') {
+			
+			$id=$request->param('id');
+			$result=0;
+			$result=$count->deleteInfo($id);
+			
+			if($result==0){
+				$this->error('操作失败，请重试');
+			} else {
+				$this->success('操作成功', '/admin/count/index/');
+			}
+		}
+		
+		$this->error('非法操作，请重试');
+	}
 }
---
+++ database/migrations/20170902035522_create_count_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class CreateCountTable extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+	public function up()
+	{
+	  $this->table('count',['engine'=>'MyISAM'])
+	    ->addColumn(Column::string('username')->setComment('用户名'))
+	    ->addColumn(Column::string('ipaddr')->setComment('IP地址'))
+	    ->addColumn(Column::integer('start_time')->setComment('访问开始时间'))
+	    ->addColumn(Column::integer('end_time')->setComment('结束时间')) 
+	    ->addColumn(Column::integer('online_time')->setComment('在线时长')) 
+	    ->addColumn(Column::integer('user_id')->setComment('用户ID')) 
+ 		->addColumn(Column::integer('status')->setComment('状态'))
+ 	    ->addColumn(Column::integer('create_time')->setComment('添加时间'))
+	    ->create();
+	}
+	
+	/**
+	* Down Method.
+	*/
+	public function down()
+	{
+	    $this->dropTable('information_extend');
+	}
+}
