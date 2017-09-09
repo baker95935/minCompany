27cefb74872bf5085aa4dd6d9bc5e9f99a2fa639
baker95935 <baker95935@qq.com>
2017-09-09 14:30:30 +0800

    api interface

---
+++ application/index/controller/Api.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\Request;
+use app\admin\model\Information as informationModel;
+use app\admin\model\InformationExtend as infoEModel;
+
+class Api extends Controller
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function information()
+    {
+        $information=new informationModel();
+		$infoE=new infoEModel();
+		$request = request();
+		
+		$result=array();
+		
+		$id=$request->param('id');
+		if(!empty($id)) {
+	    	$data=$hotList=array();
+			$data=informationModel::get($id);
+			//$hotList=$infoE->getListInfo(array('iid'=>$data['id']),array());
+		}
+		echo json_encode($data);
+    }
+
+  
+}
