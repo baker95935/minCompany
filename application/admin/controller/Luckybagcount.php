9166587a84b1fc69a3ec34fe1e9bb6fd9f2726eb
baker95935 <baker95935@qq.com>
2018-03-04 09:04:11 +0800

    error done

--- application/admin/controller/Luckybagcount.php
+++ application/admin/controller/Luckybagcount.php
@@ -50,7 +50,7 @@ class Luckybagcount extends Common
 		    ->field('scene_id,count(id) as pv,count(distinct ipaddr) as uv,SUM(luckybag_click) AS luckybag_click_total,SUM(adviser_click) AS adviser_click_total,SUM(testdrive_click) AS testdrive_click_total,SUM(buy_click) AS buy_click_total,SUM(activity_click) AS activity_click_total,SUM(finance_click) AS finance_click_total,SUM(substitution_click) AS substitution_click_total')
 		    ->group('scene_id ASC')
 		    ->where($data)
-		    ->order('scene_id ASC');
+		    ->order('scene_id ASC')
 		    ->paginate(20);
 			
 		if(!$scene_id) {
